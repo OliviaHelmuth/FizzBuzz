8674f90ada46c1fcfce7816b783c018ba82a5b84
Olivia Helmuth <Olivia.Helmuth@pm.me>
2020-07-08 23:33:32 +0200

    enhance logic

--- Js.js
+++ Js.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         num: 0,
         output:null,
-        fizzBuzzCounter: null,        
+        fizzBuzzCounter: null, 
+        showIt: false,
     },
     methods: {
         fizzBuzz() {
@@ -12,23 +13,30 @@ new Vue({
             this.fizzBuzzCounter = setInterval(this.counter, 1000)
         },
         counter() {
+            this.showIt = false;
             this.num++;
             if (this.num == 101) {
                 clearInterval(this.fizzBuzzCounter);
             }
             else if (this.num % 15 == 0) {
                 this.output = "FizzBuzz!";
+                this.showIt = true;
             }
             else if (this.num % 3 == 0) {
                 this.output = "Fizz!";
+                this.showIt = true;
             }
             else if (this.num % 5 == 0) {
                 this.output = "Buzz!";
+                this.showIt = true;
             }
             else {
                 this.output = this.num;
+                this.showIt = true;
             }
         }
+    },
+    computed: {
 
     }
 })
